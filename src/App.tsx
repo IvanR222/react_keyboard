85f671edaad247fba7c16afd9181720fef4aab9c
Ivan <rusev065@gmail.com>
2025-11-17 11:00:27 +0200

    extend a class component like as task requires

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 
-export const App: React.FC = () => {
-  const [pressedKey, setPressedKey] = useState<string | null>(null);
-  useEffect(() => {
-    const handleKeyUp = (event: KeyboardEvent) => {
-      setPressedKey(event.key);
-    };
-    document.addEventListener('keyup', handleKeyUp);
-
-    return () => {
-      document.removeEventListener('keyup', handleKeyUp);
-    };
-  }, []);
-
-  return (
-    <div className='App'>
-      <p className='App__message'>
-        {pressedKey === null
-        ? 'Nothing was pressed yet'
-        : `The last pressed key is [${pressedKey}]`}
-      </p>
-    </div>
-  );
+type State = {
+  pressedKey: string | null;
 };
+
+export class App extends React.Component<{}, State> {
+  state: Readonly<State> = {
+    pressedKey: null,
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp);
+  }
+
+  render() {
+    const { pressedKey } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">
+          {pressedKey === null
+            ? 'Nothing was pressed yet'
+            : `The last pressed key is [${pressedKey}]`}
+        </p>
+      </div>
+    );
+  }
+}
+export default App;
